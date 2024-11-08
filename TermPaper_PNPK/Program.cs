e4e296788af5bb4c0913c7b8fcd12acae237b94f
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2024-11-08 18:06:16 +0300

    Созданы структутры Book, Genre, AcquisitionMethod, Publisher. Создан метод Menu как гланое меню для взаимодействия со структурами.  Созданы метод для проверки пункта меню, вводимого пользователем, и  метод для вывода текста

--- TermPaper_PNPK/Program.cs
+++ TermPaper_PNPK/Program.cs
@@ -10,7 +10,88 @@ namespace TermPaper_PNPK
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test commit");
+            Menu();
         }
+
+        public struct Book
+        {
+            public int Id;
+            public string Author;
+            public string Title;
+            public string Genre;
+            public string Publisher;
+            public int Year;
+            public int VolumeCount;
+            public string AcquisitionMethod;
+            public decimal Price;
+            public string ReaderFullName;
+            public string Notes;
+        }
+
+        public struct Genre
+        {
+            public string Name;
+        }
+
+        public struct AcquisitionMethod
+        {
+            public string Name;
+        }
+
+        static void TextOutput(string text)        
+        {
+            Console.Write(text);
+        }
+
+        static void MenuTextOutput()
+        {
+            TextOutput("Выберите действие для работы с книгой");
+            TextOutput("Нажмите 1 для добавления данных о книге");
+            TextOutput("Нажмите 2 для изменения данных о книге");
+            TextOutput("Нажмите 3 для удаления данных о книге");
+            TextOutput("Нажмите 4 для поиска книг");
+            TextOutput("Нажмите 5 для сортировки книг");
+            TextOutput("Для выхода нажмите 6");
+        }
+
+        static void Menu()
+        {
+            MenuTextOutput();
+            int menuItem = GetIntPositiveDigit();
+
+            switch (menuItem)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+            }
+
+
+
+        }
+
+
+        static int GetIntPositiveDigit()
+        {
+            int value;
+
+            while (!int.TryParse(Console.ReadLine(), out value) || value < 1 || value > 6)
+            {
+                Console.WriteLine("Введена не существующая операция. Введите заново. ");
+            }
+
+            return value;
+        }
+
     }
 }
